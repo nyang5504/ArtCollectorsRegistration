9d59771b300a834f16b78480946ef4de9bd71ad6
nyang5504 <ninayang3.14@gmail.com>
2024-07-10 12:02:12 -0400

    commit

--- src/main/java/com/yang/artcollectorsregistration/entity/ArtCollector.java
+++ src/main/java/com/yang/artcollectorsregistration/entity/ArtCollector.java
@@ -3,24 +3,123 @@ package com.yang.artcollectorsregistration.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
 @Table(name="art_collectors")
 public class ArtCollector {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
             name ="art_collectors_roles",
-            joinColumns = {@JoinColumn(name = "ART_COLLECTOR_ID", referencedColumnName = "ID")},
+            joinColumns = {@JoinColumn(name = "ART_COLLECTORS_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name="ROLE_ID", referencedColumnName = "ID")}
     )
-    private List<Role> roles;
+    private List<Role> roles = new ArrayList<>();
+
+    public ArtCollector(){}
+
+    public ArtCollector(String name, String email, String password, List<Role> roles) {
+        this.username = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public ArtCollector(Long id, String name, String email, String password, List<Role> roles) {
+        this.id = id;
+        this.username = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "ArtCollector{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
 }
+
+
+
+//@Data
+//@Entity
+//@Table(name="art_collectors")
+//public class ArtCollector {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String username;
+//    private String email;
+//    private String password;
+//
+//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinTable(
+//            name ="art_collectors_roles",
+//            joinColumns = {@JoinColumn(name = "ART_COLLECTOR_ID", referencedColumnName = "ID")},
+//            inverseJoinColumns = {@JoinColumn(name="ROLE_ID", referencedColumnName = "ID")}
+//    )
+//    private List<Role> roles;
+//}
--- src/main/java/com/yang/artcollectorsregistration/entity/Role.java
+++ src/main/java/com/yang/artcollectorsregistration/entity/Role.java
@@ -6,13 +6,75 @@ import lombok.Data;
 import java.util.List;
 
 @Entity
-@Data
 @Table(name = "roles")
 public class Role {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
     @ManyToMany(mappedBy = "roles")
     private List<ArtCollector> artCollectors;
+
+    public Role(){}
+
+    public Role(String name, List<ArtCollector> artCollectors) {
+        this.name = name;
+        this.artCollectors = artCollectors;
+    }
+    public Role(Long id, String name, List<ArtCollector> artCollectors) {
+        this.id = id;
+        this.name = name;
+        this.artCollectors = artCollectors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ArtCollector> getUsers() {
+        return artCollectors;
+    }
+
+    public void setUsers(List<ArtCollector> artCollectors) {
+        this.artCollectors = artCollectors;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", artCollectors=" + artCollectors +
+                '}';
+    }
 }
+
+
+//@Entity
+//@Data
+//@Table(name = "roles")
+//public class Role {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//    @ManyToMany(mappedBy = "roles")
+//    private List<ArtCollector> artCollectors;
+//}
